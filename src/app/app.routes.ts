95686cd80d96a2aed3b8d2aef5a8892b1495e145
fparker4th <fparker@deliveryman.com>
2026-07-21 16:57:23 -0400

    nice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TaskManager } from './task-manager/task-manager';
+import { Home } from './home/home';
 
 export const routes: Routes = [
     { path: 'dashboard', component: TaskManager },
+    {path: 'home', component: Home },
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: '**', redirectTo: '/dashboard' }
 ];
