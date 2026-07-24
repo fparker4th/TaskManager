41931270259accffc38f86bb3e9e9941da11354c
fparker4th <fparker@deliveryman.com>
2026-07-24 15:36:28 -0400

    svae\

--- src/app/services/task-stats-service.ts
+++ src/app/services/task-stats-service.ts
@@ -1,29 +1,32 @@
-import { Service } from '@angular/core';
+import { inject, Service } from '@angular/core';
 import { Task } from '../model/task';
+import { TaskManagerService } from './task-manager-service';
 
 @Service()
 export class TaskStatsService {
-    getCompletedTasksCount(tasks: Task[]): number {
-        return tasks.filter(task => task.status === 'completed').length;
+    taskMangerService: TaskManagerService = inject(TaskManagerService);
+    getCompletedTasksCount(): number {
+        return this.taskMangerService.getTasks().filter(task => task.status === 'completed').length;
     }
 
-    getPendingTasksCount(tasks: Task[]): number {
-        return tasks.filter(task => task.status === 'pending').length;
+    getPendingTasksCount(): number {
+        return this.taskMangerService.getTasks().filter(task => task.status === 'pending').length;
     }
 
-    getOverdueTasksCount(tasks: Task[]): number {
+    getOverdueTasksCount(): number {
         const today = new Date();
         today.setHours(0, 0, 0, 0);
-        return tasks.filter(task => new Date(task.dueDate) < today && task.status != 'completed').length;
+        return this.taskMangerService.getTasks().filter(task => new Date(task.dueDate) < today && task.status != 'completed').length;
     }
 
-    getCompletionRate(tasks: Task[]): number {
+    getCompletionRate(): number {
+        const tasks:Task[] = this.taskMangerService.getTasks()
         if (tasks.length == 0) return 0;
-        return Math.round((this.getCompletedTasksCount(tasks) / tasks.length) * 100);
+        return Math.round((this.getCompletedTasksCount() / tasks.length) * 100);
     }
 
-    getProductivityLevel(tasks: Task[]): string {
-        const rate = this.getCompletionRate(tasks);
+    getProductivityLevel(): string {
+        const rate = this.getCompletionRate();
         if (rate >= 80) return 'excellent';
         if (rate >= 60) return 'good';
         if (rate >= 40) return 'needs-improvement';
